b5c21b3ea07c0375929040319580c94e47988d9b
Nafis Rayan <nafisrayan123@gmail.com>
2024-09-11 00:12:33 +0600

    "Comment out userRouter import and route"

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRouter = require('./routes/auth');
-const userRouter = require('./routes/user');
+// const userRouter = require('./routes/user');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRouter);
-app.use('/api', userRouter); // Ensure this is the correct route
+// app.use('/api', userRouter); // Ensure this is the correct route
 
 mongoose.connect('mongodb+srv://vaugheu:Mpassword123@mern-temp.ioj58.mongodb.net/?retryWrites=true&w=majority&appName=mern-temp', {
   useNewUrlParser: true,
